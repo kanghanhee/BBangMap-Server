bc015f09a8a5a56ee594fc4ffdecb2e474d333cb
kanghanhee <kanghanhee59@gmail.com>
2021-11-14 22:04:55 +0900

    update: 후기 dto 수정

--- BBangMap-Server/src/review/dto/reviewDto.js
+++ BBangMap-Server/src/review/dto/reviewDto.js
@@ -5,6 +5,9 @@ const reviewDto = (review) => {
     content: review.content,
     reviewImg: review.reviewImgList[0],
     reviewCreatedDate: review.createdAt,
+    isOnline: review.isOnline,
+    isVegan: review.isVegan,
+    purchase: review.purchaseBreadList,
   };
 };
 
