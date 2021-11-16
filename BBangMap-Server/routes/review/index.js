f481f19fb903d99b0c1cd9081703612c337ca244
kanghanhee <kanghanhee59@gmail.com>
2021-11-16 19:17:54 +0900

    update: 후기 보관하기 경로명 변경

--- BBangMap-Server/routes/review/index.js
+++ BBangMap-Server/routes/review/index.js
@@ -6,5 +6,6 @@ const reviewController = require("../../src/review/controller");
 router.get("/", authUtil.checkUuid, reviewController.reviewAll);
 router.get("/search", authUtil.checkUuid, reviewController.reviewSearch);
 router.get("/detail", authUtil.checkUuid, reviewController.reviewDetail);
+router.get("/storage", authUtil.checkUuid, reviewController.storedReviewList);
 
 module.exports = router;
