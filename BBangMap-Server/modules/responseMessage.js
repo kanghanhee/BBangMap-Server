a3f42209f0dfd260218f7c714eac31c597a5b57f
kanghanhee <kanghanhee59@gmail.com>
2021-11-16 21:01:11 +0900

    feat: 후기 responseMessage 등록

--- BBangMap-Server/modules/responseMessage.js
+++ BBangMap-Server/modules/responseMessage.js
@@ -1,16 +1,20 @@
 module.exports = {
-    NULL_VALUE: "필요한 값이 없습니다.",
-    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
-    UNAUTHORIZED: "권한없음",
+  NULL_VALUE: "필요한 값이 없습니다.",
+  OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+  UNAUTHORIZED: "권한없음",
 
-    /* 서버에러 */
-    INTERNAL_SERVER_ERROR: "서버 내부 오류",
+  /* 서버에러 */
+  INTERNAL_SERVER_ERROR: "서버 내부 오류",
 
-    /*토큰*/
-    INVALID_UUID: "존재하지 않는 UUID입니다.",
+  /*토큰*/
+  INVALID_UUID: "존재하지 않는 UUID입니다.",
 
-    /*빵집*/
-    SUCCESS_GET_BAKERY: "빵집불러오기 성공",
-    SUCCESS_GET_BAKERY_IMG: "빵집 이미지 불러오기 성공",
-    SUCCESS_SAVED_BAKERY : "빵집 보관하기 성공"
-}
+  /*빵집*/
+  SUCCESS_GET_BAKERY: "빵집불러오기 성공",
+  SUCCESS_GET_BAKERY_IMG: "빵집 이미지 불러오기 성공",
+  SUCCESS_SAVED_BAKERY: "빵집 보관하기 성공",
+
+  /*후기*/
+  SUCCESS_GET_REVIEW: "후기불러오기 성공",
+  SUCCESS_SAVED_REVIEW: "후기 보관하기 성공",
+};
