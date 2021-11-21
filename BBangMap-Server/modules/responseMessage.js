18ae2fd19b78cf719fbbd87813e881bf7f87fa69
soleu <dlthf555@gmail.com>
2021-11-22 00:32:08 +0900

    refacotor : merge conflict

--- BBangMap-Server/modules/responseMessage.js
+++ BBangMap-Server/modules/responseMessage.js
@@ -6,19 +6,20 @@ module.exports = {
   /* 서버에러 */
   INTERNAL_SERVER_ERROR: "서버 내부 오류",
 
-    INVALID_TOKEN: "유효하지 않은 토큰",
-    
   /*토큰*/
   INVALID_UUID: "존재하지 않는 UUID입니다.",
-    EMPTY_TOKEN: "빈 토큰",
-    EXPIRED_TOKEN: "토큰 기간 만료",
-    INVALID_TOKEN: "유효하지 않은 토큰",
-    INVALID_UUID: "존재하지 않는 UUID입니다.",
+  INVALID_TOKEN: "유효하지 않은 토큰",
+
+  EMPTY_TOKEN: "빈 토큰",
+  EXPIRED_TOKEN: "토큰 기간 만료",
+  INVALID_TOKEN: "유효하지 않은 토큰",
+  INVALID_UUID: "존재하지 않는 UUID입니다.",
+
   /*빵집*/
   SUCCESS_GET_BAKERY: "빵집불러오기 성공",
   SUCCESS_GET_BAKERY_IMG: "빵집 이미지 불러오기 성공",
   SUCCESS_SAVED_BAKERY: "빵집 보관하기 성공",
-   SUCCESS_REGISTRATION_BAKERY: "빵집 등록 성공",
+  SUCCESS_REGISTRATION_BAKERY: "빵집 등록 성공",
 
   /*후기*/
   SUCCESS_GET_REVIEW: "후기불러오기 성공",
@@ -36,14 +37,10 @@ module.exports = {
   ALREADY_USER: "이미 존재하는 회원입니다.",
   ALREADY_NICKNAME: "이미 존재하는 닉네임입니다.",
 
-    /*미션*/
-    SUCCESS_CREATE_MISSION: "미션 추가 성공",
-    SUCCESS_GET_MONTHLY_MISSION: "이달의 미션 메인 조회 성공",
-    SUCCESS_GET_SUCCEEDED_MISSION: "달성한 미션 조회 성공",
-    SUCCESS_GET_USER_RANK: "등급 조회 성공",
-    /*빵집*/
-    SUCCESS_GET_BAKERY: "빵집불러오기 성공",
-    SUCCESS_GET_BAKERY_IMG: "빵집 이미지 불러오기 성공",
-    SUCCESS_SAVED_BAKERY : "빵집 보관하기 성공",
-    SUCCESS_REGISTRATION_BAKERY : "빵집 등록 성공"
+  /*미션*/
+  SUCCESS_CREATE_MISSION: "미션 추가 성공",
+  SUCCESS_GET_MONTHLY_MISSION: "이달의 미션 메인 조회 성공",
+  SUCCESS_GET_SUCCEEDED_MISSION: "달성한 미션 조회 성공",
+  SUCCESS_GET_USER_RANK: "등급 조회 성공",
+
 }
