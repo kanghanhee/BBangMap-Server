4ff95c736cc45eed562825e432e6c166c6560080
hyunjong-96 <ahajongs@gmail.com>
2021-12-20 23:57:16 +0900

    fix : isVisitedBakery 필드 추가 in bakeryMapDto

--- BBangMap-Server/src/bakery/dto/bakeryMapDto.js
+++ BBangMap-Server/src/bakery/dto/bakeryMapDto.js
@@ -1,9 +1,10 @@
-const bakeryMapDto = (bakery, savedBakeryList) => {
+const bakeryMapDto = (bakery, savedBakeryList, visitedBakeryList) => {
     return {
         bakeryId: bakery.id,
         latitude: bakery.latitude,
         longitude: bakery.longitude,
-        isSaveBakery: !!savedBakeryList.includes(bakery.id)
+        isSaveBakery: savedBakeryList.includes(bakery.id),
+        isVisitedBakery : visitedBakeryList.includes(bakery.id)
     }
 }
 
