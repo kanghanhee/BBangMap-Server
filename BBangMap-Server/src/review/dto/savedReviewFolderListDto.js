1d0c827c81f90e8b4031dec7583b022a4b62e2df
kanghanhee <kanghanhee59@gmail.com>
2021-12-22 23:52:05 +0900

    fix: 리뷰 전체 개수 수정

--- BBangMap-Server/src/review/dto/savedReviewFolderListDto.js
+++ BBangMap-Server/src/review/dto/savedReviewFolderListDto.js
@@ -1,7 +1,12 @@
 const savedReviewFolderDto = require('./savedReviewFolderDto');
 
-const savedReviewFolderListDto = (savedReviewFolderList, savedReviewCountList) => {
-  return savedReviewFolderList.map(savedReviewFolder => savedReviewFolderDto(savedReviewFolder, savedReviewCountList));
+const savedReviewFolderListDto = (savedReviewFolderList, savedReviewCountList, totalCount) => {
+  return {
+    reviewList: savedReviewFolderList.map(savedReviewFolder =>
+      savedReviewFolderDto(savedReviewFolder, savedReviewCountList),
+    ),
+    totalCount: totalCount,
+  };
 };
 
 module.exports = savedReviewFolderListDto;
