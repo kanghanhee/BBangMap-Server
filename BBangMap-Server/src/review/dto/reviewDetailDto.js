d39acda1f19b31cdf470239b63c3dbd9166c407a
kanghanhee <kanghanhee59@gmail.com>
2021-12-23 21:26:44 +0900

    fix: 리뷰 상세보기 dto 수정

--- BBangMap-Server/src/review/dto/reviewDetailDto.js
+++ BBangMap-Server/src/review/dto/reviewDetailDto.js
@@ -3,8 +3,12 @@ const reviewUtil = require('../utils');
 const reviewDetailDto = async (review, savedReviewList, myReviewList, likedReviewList, likeReviewCount) => {
   return {
     reviewId: review.id,
+    bakeryId: review.BakeryId,
     bakeryName: review.BakeryId === null ? '빵집 정보 없음' : review.Bakery.bakeryName,
     purchaseBreadList: review.purchaseBreadList,
+    star: review.star,
+    isOnline: review.isOnline,
+    isVegan: review.isVegan,
     content: review.content,
     reviewImg: review.reviewImgList.length < 1 ? null : review.reviewImgList,
     reviewerName: review.UserId === null ? '존재하지 않는 닉네임' : review.User.nickName,
