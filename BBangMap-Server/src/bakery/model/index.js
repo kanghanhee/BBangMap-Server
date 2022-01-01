0fd6637a675075a4e00de7d27d64ebdb54af3645
soleu <dlthf555@gmail.com>
2022-01-01 15:55:49 +0900

    fix: bakery model size 수정

--- BBangMap-Server/src/bakery/model/index.js
+++ BBangMap-Server/src/bakery/model/index.js
@@ -1,79 +1,83 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('Bakery', {
-        bakeryName: {
-            type: DataTypes.STRING(100),
-            allowNull: false
+  return sequelize.define(
+    'Bakery',
+    {
+      bakeryName: {
+        type: DataTypes.STRING(500),
+        allowNull: false,
+      },
+      openTime: {
+        type: DataTypes.STRING(1000),
+        allowNull: false,
+      },
+      offDay: {
+        type: DataTypes.STRING(1000),
+        set: function (val) {
+          return this.setDataValue('offDay', JSON.stringify(val));
         },
-        openTime: {
-            type: DataTypes.STRING(20),
-            allowNull: false
+        get: function () {
+          return JSON.parse(this.getDataValue('offDay'));
         },
-        offDay: {
-            type: DataTypes.STRING,
-            set: function (val) {
-                return this.setDataValue('offDay', JSON.stringify(val));
-            },
-            get: function () {
-                return JSON.parse(this.getDataValue('offDay'));
-            }
+      },
+      seasonMenu: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      isOnline: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      isDrink: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      isVegan: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      bestMenu: {
+        type: DataTypes.STRING(1000),
+        set: function (val) {
+          return this.setDataValue('bestMenu', JSON.stringify(val));
         },
-        seasonMenu: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
+        get: function () {
+          return JSON.parse(this.getDataValue('bestMenu'));
         },
-        isOnline: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
+      },
+      totalMenu: {
+        type: DataTypes.STRING(1000),
+        set: function (val) {
+          return this.setDataValue('totalMenu', JSON.stringify(val));
         },
-        isDrink: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
+        get: function () {
+          return JSON.parse(this.getDataValue('totalMenu'));
         },
-        isVegan: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
+      },
+      address: {
+        type: DataTypes.STRING(1000),
+        allowNull: false,
+      },
+      latitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      longitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      bakeryImg: {
+        type: DataTypes.STRING(2000),
+        set: function (val) {
+          return this.setDataValue('bakeryImg', JSON.stringify(val));
         },
-        bestMenu: {
-            type: DataTypes.STRING,
-            set: function (val) {
-                return this.setDataValue('bestMenu', JSON.stringify(val));
-            },
-            get: function () {
-                return JSON.parse(this.getDataValue('bestMenu'));
-            }
+        get: function () {
+          return JSON.parse(this.getDataValue('bakeryImg'));
         },
-        totalMenu: {
-            type: DataTypes.STRING,
-            set: function (val) {
-                return this.setDataValue('totalMenu', JSON.stringify(val));
-            },
-            get: function () {
-                return JSON.parse(this.getDataValue('totalMenu'));
-            }
-        },
-        address:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
-        latitude:{
-            type:DataTypes.DOUBLE,
-            allowNull:false
-        },
-        longitude:{
-            type:DataTypes.DOUBLE,
-            allowNull:false
-        },
-        bakeryImg:{
-            type:DataTypes.STRING(1000),
-            set:function(val){
-                return this.setDataValue('bakeryImg',JSON.stringify(val));
-            },
-            get:function(){
-                return JSON.parse(this.getDataValue('bakeryImg'));
-            }
-        }
-    }, {
-        freezeTableName: true,
-        timestamps: true
-    })
-}
+      },
+    },
+    {
+      freezeTableName: true,
+      timestamps: true,
+    },
+  );
+};
