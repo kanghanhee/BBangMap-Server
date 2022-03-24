43f083a8b4442e51d6faa459254b5c61da12ed68
hyunjong-96 <ahajongs@gmail.com>
2022-03-25 02:41:54 +0900

    fix : 빵집 검색 데이터 추가 - 주소 추가

--- BBangMap-Server/src/bakery/dto/bakerySearchDto.js
+++ BBangMap-Server/src/bakery/dto/bakerySearchDto.js
@@ -8,7 +8,8 @@ const bakerySearchDto = (searchBakery, latitude, longitude, visitedBakeryList) =
         latitude : searchBakery.latitude,
         distance : (latitude == null&& longitude == null) ? null : bakeryUtils.getDistance(
             searchBakery.latitude, searchBakery.longitude, latitude, longitude),
-        isVisitedBakery : visitedBakeryList.includes(searchBakery.id)
+        isVisitedBakery : visitedBakeryList.includes(searchBakery.id),
+        address : searchBakery.address
     }
 }
 
