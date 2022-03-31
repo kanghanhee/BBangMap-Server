c4fc5f2e1bc3db9ecb3117eaa201fd4a7ec4efee
hyunjong-96 <ahajongs@gmail.com>
2022-03-31 22:47:47 +0900

    fix : 카드 사진 empty 뷰 수정 - 회색 -> 검은색

--- BBangMap-Server/modules/definition.js
+++ BBangMap-Server/modules/definition.js
@@ -1,4 +1,4 @@
 module.exports = {
-  defaultBgImg: 'https://bbang-map.s3.ap-northeast-2.amazonaws.com/images/origin/img_bg_empty_pattern_375x375.png',
+  defaultBgImg: 'https://bbang-map.s3.ap-northeast-2.amazonaws.com/images/origin/bg_empty_pattern_dark.png',
   defaultProfileImg: 'https://bbang-map.s3.ap-northeast-2.amazonaws.com/images/user/default/profile_empty.png',
 };
