8fcee66ec59573a2ce4f3c844489c4b1fbe7522d
hyunjong-96 <ahajongs@gmail.com>
2022-05-29 23:43:29 +0900

    feat : 빵집 모델 데이터 추가 - 태그 : 상시, 비정기
    - 유의사항 : 주차, 예약, 노키즈, 반려동물 가능 여부
    - 기타 : 블로그, 인스타 링크

--- BBangMap-Server/src/bakery/model/index.js
+++ BBangMap-Server/src/bakery/model/index.js
@@ -1,83 +1,132 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define(
-    'Bakery',
-    {
-      bakeryName: {
-        type: DataTypes.STRING(500),
-        allowNull: false,
-      },
-      openTime: {
-        type: DataTypes.STRING(1000),
-        allowNull: false,
-      },
-      offDay: {
-        type: DataTypes.STRING(1000),
-        set: function (val) {
-          return this.setDataValue('offDay', JSON.stringify(val));
+    return sequelize.define(
+        'Bakery',
+        {
+            bakeryName: {
+                type: DataTypes.STRING(500),
+                allowNull: false,
+            },
+            openTime: {
+                type: DataTypes.STRING(1000),
+                allowNull: false,
+            },
+            offDay: {
+                type: DataTypes.STRING(1000),
+                set: function (val) {
+                    return this.setDataValue('offDay', JSON.stringify(val));
+                },
+                get: function () {
+                    return JSON.parse(this.getDataValue('offDay'));
+                },
+            },
+            bestMenu: {
+                type: DataTypes.STRING(1000),
+                set: function (val) {
+                    return this.setDataValue('bestMenu', JSON.stringify(val));
+                },
+                get: function () {
+                    return JSON.parse(this.getDataValue('bestMenu'));
+                },
+            },
+            totalMenu: {
+                type: DataTypes.STRING(1000),
+                set: function (val) {
+                    return this.setDataValue('totalMenu', JSON.stringify(val));
+                },
+                get: function () {
+                    return JSON.parse(this.getDataValue('totalMenu'));
+                },
+            },
+            address: {
+                type: DataTypes.STRING(1000),
+                allowNull: false,
+            },
+            latitude: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+            },
+            longitude: {
+                type: DataTypes.DOUBLE,
+                allowNull: false,
+            },
+            bakeryImg: {
+                type: DataTypes.STRING(2000),
+                set: function (val) {
+                    return this.setDataValue('bakeryImg', JSON.stringify(val));
+                },
+                get: function () {
+                    return JSON.parse(this.getDataValue('bakeryImg'));
+                },
+            },
+            seasonMenu: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+            },
+            isDrink: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //태그 & 온라인 여부
+            isOnline: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //태그 & 비건여부
+            isVegan: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //태그 & 상시
+            allTheTime: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //태그 & 비정기
+            irregularPeriod: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //유의사항 & 주차가능
+            isParkingAvailable:{
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //유의사항 & 어린이 출입가능 여부
+            isChildAvailable:{
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //유의사항 & 예약가능여부
+            isReservationAvailable:{
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            //유의사항 & 반려동물 출입 가능여부
+            isPetAvailable:{
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            blog: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            instagram: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
         },
-        get: function () {
-          return JSON.parse(this.getDataValue('offDay'));
+        {
+            freezeTableName: true,
+            timestamps: true,
         },
-      },
-      seasonMenu: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      isOnline: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      isDrink: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      isVegan: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-      },
-      bestMenu: {
-        type: DataTypes.STRING(1000),
-        set: function (val) {
-          return this.setDataValue('bestMenu', JSON.stringify(val));
-        },
-        get: function () {
-          return JSON.parse(this.getDataValue('bestMenu'));
-        },
-      },
-      totalMenu: {
-        type: DataTypes.STRING(1000),
-        set: function (val) {
-          return this.setDataValue('totalMenu', JSON.stringify(val));
-        },
-        get: function () {
-          return JSON.parse(this.getDataValue('totalMenu'));
-        },
-      },
-      address: {
-        type: DataTypes.STRING(1000),
-        allowNull: false,
-      },
-      latitude: {
-        type: DataTypes.DOUBLE,
-        allowNull: false,
-      },
-      longitude: {
-        type: DataTypes.DOUBLE,
-        allowNull: false,
-      },
-      bakeryImg: {
-        type: DataTypes.STRING(2000),
-        set: function (val) {
-          return this.setDataValue('bakeryImg', JSON.stringify(val));
-        },
-        get: function () {
-          return JSON.parse(this.getDataValue('bakeryImg'));
-        },
-      },
-    },
-    {
-      freezeTableName: true,
-      timestamps: true,
-    },
-  );
+    );
 };
