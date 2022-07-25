9d9ef5f164bbec5b3eee91423144ccfc203d1f05
hyunjong-96 <ahajongs@gmail.com>
2022-07-25 23:52:22 +0900

    fix : app_version 최소 1.0 (중간 버전까지) 이상이 오도록 validation 추가

--- BBangMap-Server/src/review/controller/index.js
+++ BBangMap-Server/src/review/controller/index.js
@@ -99,6 +99,10 @@ module.exports = {
 
         const appVersion = req.header.appVersion;
 
+        if(appVersion === -1){
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.INVALID_APP_VERSION));
+        }
+
         let files = [];
         if (req.files['reviewImgList']) files = req.files['reviewImgList'];
 
