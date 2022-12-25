c70f0fff9595cac5bb971848c5320e8915d5da7b
soleu <dlthf555@gmail.com>
2022-12-26 07:53:42 +0900

    health check logic 추가

---
+++ BBangMap-Server/src/health/controller/index.js
@@ -0,0 +1,16 @@
+const util = require('../../../modules/util');
+const statusCode = require('../../../modules/statusCode');
+const responseMessage = require('../../../modules/responseMessage');
+
+module.exports = {
+  getHealth: async (req, res) => {
+    try {  
+        return res
+            .status(statusCode.OK)
+            .send(util.success(statusCode.OK, "OK", ""));
+    } catch (err) {
+        return res.status(statusCode.INTERNAL_SERVER_ERROR)
+            .send(util.fail(statusCode.FORBIDDEN,err,""));
+        }
+    }
+};
