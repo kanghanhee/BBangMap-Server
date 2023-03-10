edab60635c9aeb4615176072a223b4adee78d187
kanghanhee <kanghanhee59@gmail.com>
2023-03-10 16:14:55 +0900

    fix : 후기 전체보기와 기존 dto 분리

--- BBangMap-Server/src/review/dto/reviewListDto.js
+++ BBangMap-Server/src/review/dto/reviewListDto.js
@@ -1,9 +1,17 @@
 const reviewDto = require('./reviewDto');
+const reviewAllDto = require('./reviewAllDto');
 
-const reviewListDto = (reviewList) => {
+const reviewListDto = (reviewList, userId) => {
   return reviewList.map(review => {
     return reviewDto.detailDto(review);
   });
 };
 
-module.exports = reviewListDto;
+// 후기 전체보기 raw query로 불러온 데이터들
+const reviewAllListDto = reviewList => {
+  return reviewList.map(review => {
+    return reviewAllDto.detailDto(review);
+  });
+};
+
+module.exports = { reviewListDto, reviewAllListDto };
