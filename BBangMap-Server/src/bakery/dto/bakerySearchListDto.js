5e63885cc5f267dd779cf5123bd4ce6836378ca4
kanghanhee <kanghanhee59@gmail.com>
2023-07-17 10:08:33 +0900

    async 키워드 삭제

--- BBangMap-Server/src/bakery/dto/bakerySearchListDto.js
+++ BBangMap-Server/src/bakery/dto/bakerySearchListDto.js
@@ -1,7 +1,7 @@
 const bakerySearchDto = require('./bakerySearchDto');
 
-const bakerySearchListDto = async (searchBakeryList, latitude, longitude, visitedBakeryList) => {
-  return await Promise.all(
+const bakerySearchListDto = (searchBakeryList, latitude, longitude, visitedBakeryList) => {
+  return Promise.all(
     searchBakeryList.map(searchBakery => {
       return bakerySearchDto(searchBakery, latitude, longitude, visitedBakeryList);
     }),
